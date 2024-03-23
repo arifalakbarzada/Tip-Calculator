24061ee426c9414288eff1615bc72ad752f1a0b9
arif2004js <yearif689@gmail.com>
2024-03-23 13:39:44 +0400

    JS and tip logic finished

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 const inputAmount = document.querySelector("#amount");
-const inputPercent = document.querySelector("#percent");
 const peopleInput = document.querySelector("#people");
 const calculateBtn = document.querySelector("#calc");
 const tip = document.querySelector("#tip");
 const total = document.querySelector("#total");
 const btnReset = document.querySelector("#reset");
-let personAmount = document.querySelector("#perPerson");
-const results = document.querySelector(".results");
-results.style.display =  "none";
+
+let percents = document.querySelectorAll('.value');
+let inputPercent = document.querySelectorAll(".value");
+
+percents.forEach(function(input) {
+    input.addEventListener('click', function(event) {
+        console.log("Hello");
+        let clickedInput = event.target;
+        
+        if (!clickedInput.classList.contains('active')) {
+            percents.forEach(function(inp) {
+                inp.classList.remove('active');
+            });
+            clickedInput.classList.add('active');
+        }
+    });
+});
+
+
+
+
 function reset() {
     inputAmount.value = "";
     inputPercent.value = "";
@@ -16,25 +33,33 @@ function reset() {
 btnReset.addEventListener("click", reset);
 function calcTip() {
     let amount = +inputAmount.value;
-    let percent = +inputPercent.value;
+    let testing = /^[0-9.%]+$/;
+let percent;
+for (let i=0; i < percents.length; i++) {  
+     if (percents[i].classList.contains("active")){
+        percent = percents[i].value.slice(0, -1);
+     }
+    }
+    
+    percent = +percent / 100;
     let numPeople = +peopleInput.value;
-    let totalAmount = amount + amount/100 * percent;
-    let personTip = amount/ 100 * percent;
-    if (isNaN(amount) || isNaN(percent) || isNaN(numPeople) || amount == 0 || percent == 0 || numPeople == 0) {
+    let totalAmount = amount + amount  * percent;
+    let personTip = amount  * percent  / numPeople;
+    if (isNaN(amount) || isNaN(percent) || isNaN(numPeople) || !testing.test(percent) || amount <= 0 || percent <= 0 || numPeople <= 0) {
         alert("Please enter a valid number!");
     }
-    else if (amount > 0 && percent > 0 && numPeople > 0) {
-        tip.textContent = personTip;
-        total.textContent = totalAmount;
-        personAmount.textContent = totalAmount / numPeople;
-        results.style.display =  "flex";
-    }
     reset()
+    console.log(percent);
+    console.log(totalAmount);
+    console.log(personTip);
 }
+
 calculateBtn.addEventListener("click", calcTip);
 
-document.addEventListener('keypress', function(e){
-   if(e.key === 'Enter'){
-    calcTip();
-   }
-   });
+
+
+document.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        calcTip();
+    }
+});
